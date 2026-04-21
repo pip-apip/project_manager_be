cddad37c02cebb63e5efdcdfbafc608500d9772e
pip-apip <ardian.figo28@gmail.com>
2026-04-21 14:38:02 +0700

    Add Observers - Project Progress While Update and Create Spektek

---
+++ app/Observers/SpektekObserver.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\ActivityCategory;
+use App\Models\Project;
+
+class SpektekObserver
+{
+    /**
+     * Handle the ActivityCategory "created" event.
+     */
+    public function created(ActivityCategory $activityCategory): void
+    {
+        $project = Project::with('company', 'projectLeader')->find($activityCategory->project_id);
+
+        $project->update([
+            'progress' => $project->activityCategories()->avg('value')
+        ]);
+    }
+
+    /**
+     * Handle the ActivityCategory "updated" event.
+     */
+    public function updated(ActivityCategory $activityCategory): void
+    {
+        $project = Project::with('company', 'projectLeader')->find($activityCategory->project_id);
+
+        $project->update([
+            'progress' => $project->activityCategories()->avg('value')
+        ]);
+    }
+
+    /**
+     * Handle the ActivityCategory "deleted" event.
+     */
+    public function deleted(ActivityCategory $activityCategory): void
+    {
+        
+    }
+
+    /**
+     * Handle the ActivityCategory "restored" event.
+     */
+    public function restored(ActivityCategory $activityCategory): void
+    {
+        //
+    }
+
+    /**
+     * Handle the ActivityCategory "force deleted" event.
+     */
+    public function forceDeleted(ActivityCategory $activityCategory): void
+    {
+        //
+    }
+}
