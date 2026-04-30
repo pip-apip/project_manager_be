f5e9d28435c5b9659a8b39f513c1a35b2435ece1
pip-apip <ardian.figo28@gmail.com>
2026-04-30 14:36:10 +0700

    debug: Timelines Update

--- app/Http/Controllers/TimelinesController.php
+++ app/Http/Controllers/TimelinesController.php
@@ -116,6 +116,7 @@ class TimelinesController extends Controller
     public function update(TimelinesUpdateRequest $request, $id): JsonResponse
     {
         try {
+            dd($request->all());
             $timeline = Timeline::find($id);
 
             if (!$timeline) {
