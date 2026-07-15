ce2b72c6c9ab05fa43436411049ce331c60e4a08
pip-apip <ardian.figo28@gmail.com>
2026-07-15 19:45:52 +0700

    fix: Admin Doc Resource Response

--- app/Http/Resources/AdminDocResource.php
+++ app/Http/Resources/AdminDocResource.php
@@ -15,7 +15,8 @@ class AdminDocResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $file = rawurlencode($this->file) ? '/storage/' . rawurlencode($this->file) : '';
+        // $file = rawurlencode($this->file) ? '/storage/' . rawurlencode($this->file) : '';
+        $file = rawurlencode($this->file) ? rawurlencode($this->file) : '';
         $path = $this->file;
 
         $sizeBytes = $path && Storage::disk('public')->exists($path)
